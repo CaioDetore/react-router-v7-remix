405e74708fd3d994d5abbec5bac298c85b09b589
CaioDetore <caio.detore@sou.unifeob.edu.br>
2025-09-09 19:25:57 -0300

    cache com redis e upstash

--- app/routes/loaders.tsx
+++ app/routes/loaders.tsx
@@ -1,33 +1,10 @@
 import { Link, Outlet } from "react-router";
 import type { Route } from "./+types/loaders";
+import { getBreweries } from "~/services/beer";
 
-export type Brewery = {
-  id: string;
-  name: string;
-  brewery_type: string;
-  address_1: string;
-  address_2: string | null;
-  address_3: string | null;
-  city: string;
-  state_province: string;
-  postal_code: string;
-  country: string;
-  longitude: number;
-  latitude: number;
-  phone: string;
-  website_url: string;
-  state: string;
-  street: string;
-};
 
 export async function loader() {
-  const response = await fetch("https://api.openbrewerydb.org/v1/breweries");
-  const breweries = (await response.json()) as Brewery[];
-
-  return {
-    title: "RR7",
-    breweries,
-  };
+  return await getBreweries()
 }
 
 export default function ({ loaderData }: Route.ComponentProps) {
--- app/services/beer.ts
+++ app/services/beer.ts
@@ -1,8 +1,59 @@
-import type { Brewery } from "~/routes/loaders";
+import { cache, TTL } from "./cache";
+
+export type Brewery = {
+  id: string;
+  name: string;
+  brewery_type: string;
+  address_1: string;
+  address_2: string | null;
+  address_3: string | null;
+  city: string;
+  state_province: string;
+  postal_code: string;
+  country: string;
+  longitude: number;
+  latitude: number;
+  phone: string;
+  website_url: string;
+  state: string;
+  street: string;
+};
 
 export async function getBrewery(id: string) {
+  const cacheKey = `brewery:${id}`;
+  const cachedBrewery = await cache.get(cacheKey);
+
+  if (cachedBrewery) {
+    return JSON.parse(cachedBrewery) as Brewery;
+  }
+
   const response = await fetch(`https://api.openbrewerydb.org/v1/breweries/${id}`);
   const brewerie = (await response.json()) as Brewery;
 
+  const brewerieJson = JSON.stringify(brewerie);
+  await cache.set(cacheKey, brewerieJson);
+  await cache.expire(cacheKey, TTL);
+
   return brewerie
+}
+
+export async function getBreweries() {
+  const cachedBreweries = await cache.get('breweries')
+
+  if (cachedBreweries) {
+    return JSON.parse(cachedBreweries) as Brewery[]
+  }
+
+  const response = await fetch("https://api.openbrewerydb.org/v1/breweries");
+  const breweries = (await response.json()) as Brewery[];
+
+  const breweriesJson = JSON.stringify(breweries)
+
+  await cache.set("breweries", breweriesJson)
+  await cache.expire("breweries", TTL)
+
+  return {
+    title: "RR7",
+    breweries,
+  };
 }
